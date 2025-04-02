cec53ad54918d9c36336717f7e5995e286b9a684
alxsrobert <alexis3.robert3@gmail.com>
2025-04-02 16:24:54 +0000

    Generate age histograms

---
+++ analysis/report.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+df_input <- read_csv(
+    here::here("output", "dataset.csv.gz"),
+    col_type = cols(patient_id = col_integer(),age = col_double())
+)
+
+plot_age <- ggplot(data = df_input, aes(df_input$age)) + geom_histogram()
+
+ggsave(
+    plot = plot_age,
+    filename = "report.png", path = here::here("output"),
+)
